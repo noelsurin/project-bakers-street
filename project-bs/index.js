05a431782799121a211b43a7b633b649ed63214c
Noel Surin <57946757+noelsurin@users.noreply.github.com>
2021-03-30 01:59:14 +0530

    Add files via upload

--- project-bs/index.js
+++ project-bs/index.js
@@ -10,4 +10,23 @@ $(document).ready(function(){
         autoplayTimeout:5000,
         autoplayHoverPause:false
     });
+
+    //Category Owl Carousel
+    $("#category .owl-carousel").owlCarousel({
+        loop:true,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+
 });
